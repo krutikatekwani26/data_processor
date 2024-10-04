134c9350065ff68ec6dab13b5f39277cce9f3a2a
krutikatekwani26 <krutika.tekwani2@gmail.com>
2024-10-04 15:54:05 -0500

    not related to the package: working of union decisions with the package.

--- data_processor/__main__.py
+++ data_processor/__main__.py
@@ -1,3 +1,4 @@
+#IMPORTS
 import pandas as pd
 from data_processor.core.dataset import Dataset
 from data_processor.processors.data_cleaning_processor import DataCleaningProcessor
@@ -5,70 +6,89 @@ from data_processor.processors.data_validation_processor import DataValidationPr
 from data_processor.processors.merge_processor import MergeProcessor
 from data_processor.utils.helpers import *
 import warnings
+from .user_custom_methods import *
 warnings.filterwarnings("ignore", category=UserWarning, module="openpyxl")
 
+
 # Load dataset and schema
-dataset_path = 'data_processor/supervison_data.xlsx'
-schema_path = 'data_processor/schema.yaml'
-dataset_path2 = 'data_processor/data2.xlsx'  
-dataset_path3 = 'data_processor/data3.xlsx'
+dataset_path = 'data_processor/union_decision.xlsm'
+schema_path = 'data_processor/union_schema.yaml'
+dataset_path2 = 'data_processor/rh_union_decision.xlsm'  
+#dataset_path3 = 'data_processor/data3.xlsx'
 data = pd.read_excel(dataset_path)
 data2 = pd.read_excel(dataset_path2)
-data3 = pd.read_excel(dataset_path3)
+
+if "OVERRIDE" in data2.columns:
+    # Split the DataFrame into override and non-override parts
+    override_df = data2[data2["OVERRIDE"] == True].copy()
+    non_override_df = data2[data2["OVERRIDE"] != True].copy()
+    # Drop the OVERRIDE column from both DataFrames
+    override_df = override_df.drop("OVERRIDE", axis=1)
+    non_override_df = non_override_df.drop("OVERRIDE", axis=1)
+
+
+
 
 # Create Dataset objects
 dataset = Dataset(data, schema_path)
-dataset2 = Dataset(data2, schema_path)
-dataset3 = Dataset(data3, schema_path)
-
-# Instantiate processors
-cleaning_processor = DataCleaningProcessor()
-validation_processor = DataValidationProcessor()
-merge_processor = MergeProcessor()
-
-print(merge_processor.get_operation_list())
-
-# Add cleaning operations
-cleaning_processor.add_operation(remove_spaces_Around_punctuation)
-cleaning_processor.add_operation(manage_special_characters)
-cleaning_processor.add_operation(make_uppercase)
-cleaning_processor.add_operation(strip_leading_and_trailing_spaces)
-cleaning_processor.add_operation(clean_numeric_values)
-cleaning_processor.add_operation(remove_duplicates)
-
-# Add validation operations
-validation_processor.add_operation(drop_invalid_columns)
-validation_processor.add_operation(validate_column_values)
-
-# Add merge operation
-merge_processor.add_operation(add_new_rows)
-
-
-
-# Process cleaning
-cleaning_processor.process(dataset)
-cleaning_processor.process(dataset2)
-cleaning_processor.process(dataset3)
+dataset2 = Dataset(override_df, schema_path)
+dataset3 = Dataset(non_override_df, schema_path)
 print(dataset.get_data().shape)
 print(dataset2.get_data().shape)
 print(dataset3.get_data().shape)
 
+# Instantiate processors
+cleaning_processor = DataCleaningProcessor()
+cleaning_processor2 = DataCleaningProcessor()
+cleaning_processor3 = DataCleaningProcessor()
+validation_processor = DataValidationProcessor()
+merge_processor = MergeProcessor()
+merge_processor2 = MergeProcessor()
+
+
+# Add cleaning operations part 1
+cleaning_processor.add_operation(apply_standard_cleaning)
+cleaning_processor.add_custom_operation(convert_journeyman_to_journey)
+cleaning_processor.add_operation(remove_duplicates)
+validation_processor.add_custom_operation(check_city)
+validation_processor.add_operation(drop_invalid_columns)
+validation_processor.add_operation(validate_column_values)
+cleaning_processor2.add_custom_operation(generate_hash)
+cleaning_processor2.add_custom_operation(keep_the_largest_dup)
+merge_processor.add_custom_operation(handle_override)
+merge_processor2.add_operation(add_new_rows)
+cleaning_processor3.add_custom_operation(replace_unconfirmed)
+cleaning_processor3.add_custom_operation(keep_the_largest_dup)
 
 
 
+
+# Process cleaning
+cleaning_processor.process(dataset, dataset2,dataset3)
+print(dataset.get_data().shape)
+print(dataset2.get_data().shape)
+print(dataset3.get_data().shape)
 # Process validation
 validation_processor.process(dataset)
 validation_processor.process(dataset2)
 validation_processor.process(dataset3)
-#validated_dataset.get_data().to_csv("data_processor/midcheck_data.csv", index=False)
+cleaning_processor2.process(dataset,dataset2,dataset3)
 
 # Merge the datasets
-merged_data = merge_processor.process(dataset, dataset2,dataset3)
+merge_processor.process(dataset, dataset2)
+merge_processor2.process(dataset,dataset3)
+cleaning_processor3.process(dataset)
+
 
 # Print the merged data
-
 print(dataset.get_data().shape)
-print(dataset.get_data().head())
+print(dataset2.get_data().shape)
+print(dataset3.get_data().shape)
+
+assert not dataset.get_data()['HASH ID'].duplicated().any(), "Duplicate hash IDs found!"
+
+# Drop the 'HASH ID' column
+dataset.set_data(dataset.get_data().drop('HASH ID', axis=1))
 
 dataset.get_data().to_csv("data_processor/check_data.csv", index=False)
 
