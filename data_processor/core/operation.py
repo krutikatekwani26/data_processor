d74482aeaf2dfafeb4c16853d6e58e93023758fe
krutikatekwani26 <krutika.tekwani2@gmail.com>
2024-10-14 09:56:02 -0500

    package specific files

--- data_processor/core/operation.py
+++
@@ -1,9 +0,0 @@
-# core/operation.py
-from abc import ABC, abstractmethod
-from .dataset import Dataset
-
-
-class Operation(ABC):
-    @abstractmethod
-    def apply(self, dataset: Dataset) -> Dataset|None:
-        pass
